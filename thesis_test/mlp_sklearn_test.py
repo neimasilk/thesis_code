582a236209eca1fadf32509b539c1f9dda058c38
neimasilk <neimasilk@yahoo.com>
2016-05-07 10:15:41 +0700

    menambahkan mlp untuk diutak atik dengan dataset yang ditentukan dari ranking

---
+++ thesis_test/mlp_sklearn_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+    # test untuk mlp
+
+
+
+if __name__ == '__main__':
+    unittest.main()
